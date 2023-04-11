f39a951503386141ee860a7d55eeac7a000358a2
Pavlov Eugene <eugenepavlov523@gmail.com>
2023-04-11 16:50:57 +0300

    Just experimenting with PyVista.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+import pyvista as pv
+from pyvista import examples
 
-plt.show() 
+points = np.array([[0, 0, 0]], dtype=float)
+point_cloud = pv.PolyData(points)
+point_cloud.plot(eye_dome_lighting=True)
